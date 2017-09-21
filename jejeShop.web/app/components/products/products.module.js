13e01b87f5a7b296dac2e0c7719d58b87942a8b3
jadevine <luonsuynghi@gmail.com>
2017-09-22 01:57:51 +0700

    Bai 33 chua nhung ckedittor

--- jejeShop.web/app/components/products/products.module.js
+++ jejeShop.web/app/components/products/products.module.js
@@ -1,6 +1,6 @@
 ﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
 (function () {
-    angular.module('jejeShop.products', ['jejeShop.common']).config(config);
+    angular.module('jejeShop.products', ['jejeShop.common', 'jejeShop.product_categories']).config(config);
 
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
@@ -9,8 +9,8 @@
             url: "/products",
             templateUrl: "/app/components/products/productListView.html",
             controller: "productListController"
-        }).state('product_add', {
-            url: "/product_add",
+        }).state('add_product', {
+            url: "/add_product",
             templateUrl: "/app/components/products/productAddView.html",
             controller: "productAddController"
         });
